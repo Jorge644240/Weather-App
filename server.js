e9ccf2d229132e299e6ae3c0e62540d9e8c12196
Jorge Gallego <gallegojorge908@gmail.com>
2021-05-06 14:05:06 -0500

    App

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const https = require("https");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/static`));
 app.set("view engine", "pug");
